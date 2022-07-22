999680952e5e9bc1119bdedcd03633846c9eb2ba
J0n3s0fSt33l <91816393+J0n3s0fSt33l@users.noreply.github.com>
2022-07-22 07:03:13 -0400

    Update neosweeper.sh
    Added an output file for the full port scan

--- neosweeper.sh
+++ neosweeper.sh
@@ -17,7 +17,6 @@ fi
 echo "$TARGETIP" >> Target_list.txt
 if [ "$(echo "$TARGETIP" | awk -F '.' '{print $4}')" = "0/24" ]; then
 	echo "Commencing Network Sweep."
-	sleep 1
 
 #Performs a single ping to each IP in the network.
 	echo "Commencing Shell Ping:"
@@ -31,7 +30,7 @@ if [ "$(echo "$TARGETIP" | awk -F '.' '{print $4}')" = "0/24" ]; then
 	fping -a -g "$STATIC"0/24 1>>IP_list.txt 2>/dev/null
 	echo " " >> IP_list.txt &&echo "Fping Scan Complete."
 
-	#Performs an Nmap ping sweep.
+#Performs an Nmap ping sweep.
 	echo "Commencing Nmap Scan:"
 	nmap -sn "$STATIC"0/24 | grep "$STATIC" | cut -f 5- -d ' ' >> IP_list.txt
 	echo "Nmap Scan Complete."
@@ -40,12 +39,11 @@ if [ "$(echo "$TARGETIP" | awk -F '.' '{print $4}')" = "0/24" ]; then
 fi
 
 #For loop for Port Scanning/Service Detection:
-
-	awk '{print $1}' Target_list.txt | while read -r T; do
+awk '{print $1}' Target_list.txt | while read -r T; do
 	nmap -sC -sV -v "$T" | sudo tee Nmap-First.txt	
 	grep "open " < Nmap-First.txt | sed 's/[^.0-9][^.0-9]*/ /g' | awk '{print $1}' >> Open-ports.txt-temp
 	grep "open " < Nmap-First.txt > Services.txt-temp && echo " " >> Services.txt-temp
-	done
+done
 
 #Cleanup logs
 sort Services.txt-temp | sed '/Discovered/d' | sort -n | uniq > Services.txt
@@ -58,5 +56,5 @@ printf "Port Scan Results:\n"
 cat Open-ports.txt
 echo
 echo "Start going through the data while a full port Nmap scan runs."
-nmap -sC -sV -vv -O -p- -iL Target_list.txt
+nmap -sC -sV -vv -O -p- -oN Nmap-full.txt -iL Target_list.txt
 read -rsp "Press [ENTER] to continue."
